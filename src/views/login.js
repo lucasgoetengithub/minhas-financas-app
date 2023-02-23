39caf4741903c646804e3984e3a1851bae20e01b
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-02-23 00:11:24 -0300

    iniciando componente de login

---
+++ src/views/login.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Card from "../components/card";
+
+
+class Login extends React.Component{
+
+    render() {
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6" style={ {position : 'relative', left : '300px'} }>
+                        <div className="bs-docs-section">
+                            <Card title="login">
+                                
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
